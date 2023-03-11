c11022f408ecd1e64f67af1e147154c6979f779a
Semih Ocaklı <110246252+Semihocakli@users.noreply.github.com>
2023-03-11 22:14:35 +0300

    Update main.py

--- 007-Python_BSC/main.py
+++ 007-Python_BSC/main.py
@@ -16,3 +16,26 @@ print(f"You are currently {age} years old and you will be {age+1} years old next
 # 18 yasindan buyukse True olacak sekilde bilgisini don
 is_18_or_older = age >= 18
 print("Is your age greater than 18?", is_18_or_older)
+
+
+
+# Kullanıcı küpün kenar uzunluğunu (cm olarak alın) ve işlem numarasını girsin
+# İşlem No 1: Küpün kenarlarının toplamı (cm olarak)
+# İşlem No 2: Küpün yüzey alanının toplamı (cm^2 olarak)
+# İşlem No 3: Küpün hacmi cm^3 olarak
+
+a = int(input("Lütfen küpün bir kenar uzunluğunu cm cinsinden giriniz: "))
+secim = int(input("Lütfen işlem tipini seçiniz: \n1- Küpün kenar uzunluklarının toplamı\n2- Küpün yüzey alanlarının toplamı\n3- Küpün hacmi\n"))
+
+if secim == 1:
+    sonuc = 12 * a
+    print(f"Küpün kenar uzunluklarının toplamı: {sonuc} cm'dir.")
+elif secim == 2:
+    sonuc = 6 * a * a
+    print(f"Küpün yüzey toplamı: {sonuc} cm^2'dir.")
+elif secim == 3:
+    sonuc = a * a * a
+    print(f"Küpün hacmi: {sonuc} cm^3'dir.")
+else:
+    print("Lütfen 1-3 arasında seçim yapınız.")
+
